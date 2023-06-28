7b199197f604a97f4b961446ce6d11d5c3cc8d32
Valls <fernandodaniel.valls@externos-ar.cencosud.com>
2023-06-28 11:35:32 -0300

    en proceso

--- ejercicios_practica/1- storage/js/script.js
+++ ejercicios_practica/1- storage/js/script.js
@@ -21,33 +21,39 @@ con los datos almacenados en memoria, a fin de darle la bienvenida al usuario lo
 
 */
 
-const button = document.getElementById("btnIngresar")
+const buttonLogin = document.getElementById("btnIngresar")
+const buttonLogout = document.getElementById("btnSalir")
+
 const user = document.getElementById("usuario")
 const email = document.getElementById("email")
+
 const txtUserLogin = document.getElementById("usuarioLogeado")
 const txtEmailLogin = document.getElementById("emailLogeado")
-const login = document.getElementById("login")
 
-button.onclick = () => {
+const login = document.getElementById("login")
+const logout = document.getElementById("logout")
+
+var valueEmail = ""
+var valueUser = ""
+
+buttonLogin.onclick = () => {
     //Leo los datos ingresados en "usuario" y "email".
-    let valueEmail = email.value
-    let valueUser = user.value
+    valueEmail = user.value
+    valueUser = email.value
 
     //Almaceno en memoria permanente del explorador.
-    localStorage.setItem("user ", valueUser)
-    localStorage.setItem("email ", valueEmail)
+    localStorage.setItem("user", valueUser)
+    localStorage.setItem("email", valueEmail)
 
     //Completo las etiquetas <p> de id "usuarioLogeado" y "emailLogeado"
     txtUserLogin.textContent = valueUser
     txtEmailLogin.textContent = valueEmail
 
-    console.log(valueUser)
-    console.log(valueEmail)
+    login.classList.add("hidden")
+    logout.classList.remove("hidden")
 
+};
 
-  };
-  
-  
 /* 2 - Enunciado
 
 - Al comenzar el script se debe verificar si en memoria se encuentran
@@ -60,12 +66,12 @@ y deberá revelar la sección "logout" (quitar la clase hidden).
 */
 console.log(localStorage.length)
 
-if(localStorage.length > 0){
+if (localStorage.length > 0) {
     txtUserLogin.textContent = localStorage.getItem("user")
     txtEmailLogin.textContent = localStorage.getItem("email")
-    // login.classList.add("hidden")
+    login.classList.add("hidden")
+    logout.classList.remove("hidden")
 }
-
 
 /* 3 - Enunciado
 
@@ -76,3 +82,10 @@ volverse a cargar debería aparecer nuevamente la sección de bienvenida
 ya que no debería haber más datos en memoria cargados.
 
 */
+
+buttonLogout.onclick = () => {
+    localStorage.clear()
+    window.location.reload()
+    login.classList.remove("hidden")
+    logout.classList.add("hidden")
+}
--- ejercicios_practica/2- array methods/js/script.js
+++ ejercicios_practica/2- array methods/js/script.js
@@ -20,6 +20,18 @@ un array de personas mayores o igual a 18 años.
 
 */
 
+// console.log(personasJSON);
+
+let persons = JSON.parse(personasJSON);
+
+//Otra forma de usar filter
+let personsMayor18 = persons.filter(person => person.edad > 18);
+
+let personsMayor18_bis = persons.filter(function(person) {
+    return person.edad > 18;
+});
+
+
 /* 2 - Enunciado
 
 - Con el array de personas filtradas (resultado del enunciado anterior)
@@ -31,9 +43,34 @@ en el HTML (es decir, generar el contenido de forma dinámica).
 
 */
 
+//Traigo el elemento que voy a utilizar
+const bodyElement = document.querySelector("body");
+
+personsMayor18.forEach(function(person, index) {
+    //Creo 2 span, uno para el nombre y otro para edad
+    let nombreElemento = document.createElement("span");
+    var edadElemento = document.createElement("span");
+    //Creo 1 parrafo para agregarlos ahí
+    var parrafoElemento = document.createElement("p");
+
+    //Seteo los span con los valores del array
+    nombreElemento.textContent = person.nombre;
+    edadElemento.textContent = person.edad;
+
+    //Seteo lso valores del parrafo y le agrego los span 
+    parrafoElemento.innerHTML = "Nombre: ";
+    parrafoElemento.appendChild(nombreElemento);
+    parrafoElemento.innerHTML += "<br>Edad: ";
+    parrafoElemento.appendChild(edadElemento);
+
+    //Inserto todo dentro del body
+    bodyElement.appendChild(parrafoElemento);
+
+});
+
 /* 3 - BONUS TRACK
 
-- Si misión, en caso que decida aceptarla, es que la edad a filtrar
+- Su misión, en caso que decida aceptarla, es que la edad a filtrar
 en el primer enunciado no sea un valor escrito en este script, sino
 que sea un valor que se toma del HTML (un input).
 - Que toda la acción de filtrado (enunciado 1) y renderizado (enunciado 2)
